5a7ac4527d848d9fc68a344a1151fd16759b406c
gitdeeper07 <gitdeeper@gmail.com>
2026-03-04 05:36:44 +0000

    📚 Fix ReadTheDocs configuration

---
+++ docs/conf.py
@@ -0,0 +1,24 @@
+# Configuration file for the Sphinx documentation builder.
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+project = 'STRATICA'
+copyright = '2026, Samir Baladi'
+author = 'Samir Baladi'
+release = '1.0.0'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.mathjax',
+    'sphinx_copybutton',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,12 @@
+STRATICA Documentation
+======================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+Welcome to STRATICA's documentation!
+=====================================
+
+.. automodule:: stratica
+   :members:
